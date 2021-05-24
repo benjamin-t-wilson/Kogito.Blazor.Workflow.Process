844df704d90f5a8248e9115e38514c1e397cfd85
Benjamin Wilson <whatsup@btwicode.com>
2021-05-24 10:41:51 -0700

    add progress

---
+++ Kogito.Blazor.Workflow.Process/Entities/Domain/KafkaTopics.cs
@@ -0,0 +1,15 @@
+﻿namespace Kogito.Blazor.Workflow.Process.Entities.Domain
+{
+    public class ProcessedApplicant
+    {
+        public string id { get; set; }
+        public string source { get; set; }
+        public string type { get; set; }
+        public string time { get; set; }
+        public string data { get; set; }
+        public string kogitoProcessinstanceId { get; set; }
+        public string kogitoProcessId { get; set; }
+        public string kogitoProcessinstanceState { get; set; }
+        public string specversion { get; set; }
+    }
+}
